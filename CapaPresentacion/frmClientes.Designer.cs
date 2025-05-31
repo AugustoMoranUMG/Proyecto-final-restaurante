c91104ef4637115e8b6c044df24629fc5cf9bb17
AugustoMoranUMG <gmroana2@miumg.edu.gt>
2025-05-31 14:53:34 -0600

    Cambio a frmClientes en campo UsuarioSistema

--- CapaPresentacion/frmClientes.Designer.cs
+++ CapaPresentacion/frmClientes.Designer.cs
@@ -38,9 +38,7 @@
             this.txtNombre = new System.Windows.Forms.TextBox();
             this.label10 = new System.Windows.Forms.Label();
             this.cboxEstado = new System.Windows.Forms.ComboBox();
-            this.cboxUsuarioSistema = new System.Windows.Forms.ComboBox();
             this.txtCodigoCliente = new System.Windows.Forms.TextBox();
-            this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
@@ -67,9 +65,7 @@
             this.groupBox1.Controls.Add(this.txtNombre);
             this.groupBox1.Controls.Add(this.label10);
             this.groupBox1.Controls.Add(this.cboxEstado);
-            this.groupBox1.Controls.Add(this.cboxUsuarioSistema);
             this.groupBox1.Controls.Add(this.txtCodigoCliente);
-            this.groupBox1.Controls.Add(this.label8);
             this.groupBox1.Controls.Add(this.label9);
             this.groupBox1.Controls.Add(this.label5);
             this.groupBox1.Controls.Add(this.label4);
@@ -143,28 +139,28 @@
             "Ocasional",
             "Frecuente",
             "Exclusivo"});
-            this.cboxCategoria.Location = new System.Drawing.Point(512, 30);
+            this.cboxCategoria.Location = new System.Drawing.Point(512, 70);
             this.cboxCategoria.Name = "cboxCategoria";
             this.cboxCategoria.Size = new System.Drawing.Size(124, 21);
             this.cboxCategoria.TabIndex = 41;
             // 
             // txtTelefono
             // 
-            this.txtTelefono.Location = new System.Drawing.Point(163, 116);
+            this.txtTelefono.Location = new System.Drawing.Point(513, 24);
             this.txtTelefono.Name = "txtTelefono";
             this.txtTelefono.Size = new System.Drawing.Size(124, 20);
             this.txtTelefono.TabIndex = 40;
             // 
             // txtNit
             // 
-            this.txtNit.Location = new System.Drawing.Point(163, 88);
+            this.txtNit.Location = new System.Drawing.Point(163, 114);
             this.txtNit.Name = "txtNit";
             this.txtNit.Size = new System.Drawing.Size(124, 20);
             this.txtNit.TabIndex = 39;
             // 
             // txtNombre
             // 
-            this.txtNombre.Location = new System.Drawing.Point(163, 59);
+            this.txtNombre.Location = new System.Drawing.Point(163, 69);
             this.txtNombre.Name = "txtNombre";
             this.txtNombre.Size = new System.Drawing.Size(124, 20);
             this.txtNombre.TabIndex = 35;
@@ -173,7 +169,7 @@
             // 
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.Location = new System.Drawing.Point(19, 58);
+            this.label10.Location = new System.Drawing.Point(19, 68);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(67, 19);
             this.label10.TabIndex = 34;
@@ -185,46 +181,24 @@
             this.cboxEstado.Items.AddRange(new object[] {
             "Activo",
             "Inactivo"});
-            this.cboxEstado.Location = new System.Drawing.Point(512, 71);
+            this.cboxEstado.Location = new System.Drawing.Point(512, 114);
             this.cboxEstado.Name = "cboxEstado";
             this.cboxEstado.Size = new System.Drawing.Size(124, 21);
             this.cboxEstado.TabIndex = 33;
             // 
-            // cboxUsuarioSistema
-            // 
-            this.cboxUsuarioSistema.FormattingEnabled = true;
-            this.cboxUsuarioSistema.Items.AddRange(new object[] {
-            "Augusto Morán",
-            "Gerber Canahuí",
-            "Fernando Fajardo"});
-            this.cboxUsuarioSistema.Location = new System.Drawing.Point(512, 111);
-            this.cboxUsuarioSistema.Name = "cboxUsuarioSistema";
-            this.cboxUsuarioSistema.Size = new System.Drawing.Size(124, 21);
-            this.cboxUsuarioSistema.TabIndex = 32;
-            // 
             // txtCodigoCliente
             // 
-            this.txtCodigoCliente.Location = new System.Drawing.Point(163, 26);
+            this.txtCodigoCliente.Location = new System.Drawing.Point(163, 23);
             this.txtCodigoCliente.Name = "txtCodigoCliente";
             this.txtCodigoCliente.ReadOnly = true;
             this.txtCodigoCliente.Size = new System.Drawing.Size(124, 20);
             this.txtCodigoCliente.TabIndex = 16;
             // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(381, 110);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(110, 19);
-            this.label8.TabIndex = 6;
-            this.label8.Text = "Usuario Sistema:";
-            // 
             // label9
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(381, 70);
+            this.label9.Location = new System.Drawing.Point(381, 113);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(54, 19);
             this.label9.TabIndex = 5;
@@ -234,7 +208,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(381, 29);
+            this.label5.Location = new System.Drawing.Point(380, 69);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(75, 19);
             this.label5.TabIndex = 4;
@@ -244,7 +218,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(17, 115);
+            this.label4.Location = new System.Drawing.Point(381, 23);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(64, 19);
             this.label4.TabIndex = 3;
@@ -254,7 +228,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(17, 87);
+            this.label3.Location = new System.Drawing.Point(17, 113);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(37, 19);
             this.label3.TabIndex = 2;
@@ -264,7 +238,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(17, 27);
+            this.label2.Location = new System.Drawing.Point(17, 24);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(103, 19);
             this.label2.TabIndex = 1;
@@ -380,9 +354,7 @@
         private System.Windows.Forms.TextBox txtNombre;
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.ComboBox cboxEstado;
-        private System.Windows.Forms.ComboBox cboxUsuarioSistema;
         private System.Windows.Forms.TextBox txtCodigoCliente;
-        private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
