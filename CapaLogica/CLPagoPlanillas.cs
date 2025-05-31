9df43c0c0487ac445792ac3e26a21ae2cd887d74
AugustoMoranUMG <gmorana2@miumg.edu.gt>
2025-05-30 21:38:53 -0600

    Revert "Fernando"

--- CapaLogica/CLPagoPlanillas.cs
+++ CapaLogica/CLPagoPlanillas.cs
@@ -9,11 +9,6 @@ namespace CapaLogica
     public class CLPagoPlanillas
     {
         // Bono, MontoTotal
-        public DateTime MtdFechaSistema()
-        {
-            DateTime fechaSistema = DateTime.Now;
-            return fechaSistema;
-        }
 
         public double MtdBono (double Salario)
         {
