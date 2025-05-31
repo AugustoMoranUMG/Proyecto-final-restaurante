04690b15362bd1b938567e2e2adfee5430c45734
GerberCanahuiUMG <gerbercanahui39@gmail.com>
2025-05-30 21:05:41 -0600

    modificacion de forms

--- CapaPresentacion/FrmPagoPlanillas.cs
+++ CapaPresentacion/FrmPagoPlanillas.cs
@@ -86,5 +86,15 @@ namespace Sistema_Restaurante
         {
 
         }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
